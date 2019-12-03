1fc3021b0213d6c1c50af12b04d008b116d4150d
JackieZheng <zhengxinzhe@admans.net>
2019-12-03 10:29:00 +0800

    fix button line-height

--- CsdnCnblogReader.user.js
+++ CsdnCnblogReader.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         CSDN,CNBLOG博客阅读模式切换插件 
-// @version      2.57
+// @version      2.60
 // @description  CSDN 阅读模式和浏览模式切换，完美支持傲游、360、Chrome等浏览器
 // @author       By Jackie http://csdn.admans.cn/
 // @match        *://blog.csdn.net/*/article/details/*
@@ -14,7 +14,7 @@
 // @supportURL   https://github.com/JackieZheng/CsdnCnblogReader/issues
 // ==/UserScript==
 
-GM_addStyle("#ReadBtn{position: absolute;float: right;right: 0px;width: auto;background: #0f962191;z-index: 9999;color: white;text-align: center;margin: 5px;padding: 5px;border-radius: 5px;cursor: pointer;font-size:14px;}");
+GM_addStyle("#ReadBtn{position: absolute;float: right;right: 0px;width: auto;background: #0f962191;z-index: 9999;color: white;text-align: center;margin: 5px;padding: 5px;border-radius: 5px;cursor: pointer;font-size:14px;    line-height: 100%;}");
 GM_addStyle(".html_body_readmodel{overflow: hidden;}");
 GM_addStyle(".article_content_readmodel{position: fixed !important;top: 0px;left: 0px;width: 100%;z-index: 9999;overflow: auto !important;height: 100%;background: white;padding: 20px;border:10px solid #bce4cba8;margin: 0 !important;font-size: 150% !important;}");
 GM_addStyle(".article_content_readmodel code,.article_content_readmodel .hljs,.htmledit_views kbd,.article_content_readmodel .htmledit_views pre,.article_content_readmodel .htmledit_views code ol li div,.article_content_readmodel .htmledit_views p{font-size: 110% !important;}");
